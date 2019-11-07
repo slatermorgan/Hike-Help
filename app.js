0de78ec80dd14eea46e6555ebb6bb12c766625b3
slatermorgan <slatermorgan@hotmail.com>
2019-11-07 21:17:14 +0000

    update to use environment

--- app.js
+++ app.js
@@ -52,6 +52,6 @@ app.use(indexRoutes);
 app.use("/walks", walkRoutes);
 app.use("/walks/:id/comments", commentRoutes);
 
-app.listen(3000, process.env.IP, function () {
+app.listen(process.env.PORT, process.env.IP, function () {
     console.log("Hike Help Server Initilised" + process.env.IP);
 });
