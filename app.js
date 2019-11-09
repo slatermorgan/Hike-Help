be54e991016b592eee1e54921927237d02f50784
slatermorgan <slatermorgan@hotmail.com>
2019-11-09 11:55:08 +0000

    fix bug with database url

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes = require("./routes/comments"),
     walkRoutes = require("./routes/walks"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect(DATABASEURL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
